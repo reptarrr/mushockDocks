82287b6a749de79101fe4991fff92543edf3b67f
Jelani Abdus-Salaam <jelani0694@gmail.com>
2014-01-12 18:09:30 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MushockDocks::Application.config.secret_key_base = 'c60a99284fc9c5d661ca97475e6b4f49df4673759badcebb14e80c375cdf7875255d2be938f1aed397c06e50a2d138f92a166a90a0b91ecb7cfd452f7034b56a'
